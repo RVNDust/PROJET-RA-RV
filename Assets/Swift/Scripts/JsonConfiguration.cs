d6e79a21107611ebdfdaa9f97ffec249a4e7d5ce
MLZ <marc.lacomblez@viacesi.fr>
2019-01-14 18:10:44 +0100

    FP1-5 - Sauvegarde au travers d'une interface non-diégétique (un bouton sur la caméra) de la configuration des machines (position et rotation) dans un fichier JSON (portant comme nom la date d'enregistrement) dans le dossier streaming assets du projet
    A suivre : Préparer une interface diégétique / Réaliser la fonction de chargement de JSON en AR et VR

---
+++ Assets/Swift/Scripts/JsonConfiguration.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class JsonConfiguration : MonoBehaviour {
+
+	GameObject[] machines;
+
+	string jsonString;
+	string dataPath;
+	string json;
+	private Machine loadObject;
+
+
+	// Use this for initialization
+	void Start () {
+		machines = GameObject.FindGameObjectsWithTag("Machine");
+		dataPath = Application.dataPath + "/StreamingAssets/" + GetFileName();
+		
+	}
+
+
+	public void SaveData()
+	{
+		
+		MyList myList = new MyList();
+		myList.machinelist = new List<Machine>();
+		
+		foreach (var machine in machines)
+		{
+			Machine machineData = new Machine();
+			machineData.Name = machine.name;
+			Debug.Log(machineData.Name);
+			machineData.Position = machine.transform.position;
+			machineData.Rotation = machine.transform.rotation;
+			myList.machinelist.Add(machineData);
+		
+		}
+		
+		json = JsonUtility.ToJson(myList);
+		Debug.Log(json);
+		File.WriteAllText(dataPath, json);
+
+	}
+
+	public void LoadData()
+	{
+
+	}
+
+	public string GetFileName()
+	{
+		DateTime localDate = DateTime.Now;
+		string format = "yyyy MM dd – HH mm ss";
+
+		String myDate = localDate.ToString(format);
+		return myDate;
+		
+	}
+	
+
+}
+
+[Serializable]
+public class MyList
+{
+	public List<Machine> machinelist;
+}
+
+[Serializable]
+public class Machine
+{
+	public string Name;
+	public Vector3 Position;
+	public Quaternion Rotation;
+	
+}
