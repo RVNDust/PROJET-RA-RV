fb02d9dbdcd9241993700e20e2d1d55283b72798
PaerCode <paer763@gmail.com>
2019-01-18 17:25:29 +0100

    AR Fixed on Tablets

--- Assets/Swift/Scripts/Model/ArLoadScript.cs
+++ Assets/Swift/Scripts/Model/ArLoadScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
@@ -18,51 +19,63 @@ public class ArLoadScript : MonoBehaviour
     {
         string path;
         string jsonFile;
-        path = "jar:file://" + Application.dataPath + "!/assets/Machines.json";
+        path =
         //path = Application.dataPath + "/ADDOL/StreamingAssets" + "/Machines.json";
 
-        Debug.Log(path);
 
-        if (File.Exists(path))
+
+        path = Application.streamingAssetsPath + "/Machines.json";
+
+
+        if (Application.platform == RuntimePlatform.Android)
         {
-            jsonFile = File.ReadAllText(path);
-            if (jsonFile != null)
-            {
-                //int u = 0;
-                rootObject = JsonUtility.FromJson<RootObject>(jsonFile);
+            WWW reader = new WWW(path);
+            while (!reader.isDone) { }
 
-                for (int i = 0; i < GameObjectList.Length; i++)
-                {
-
-                    foreach (Machinelist item in rootObject.machinelist)
-                    {
-                        if (GameObjectList[i].name == item.Name)
-                        {
-                            GameObjectList[i].transform.position = new Vector3((float)item.Position.x, (float)item.Position.y, (float)item.Position.z);
-                            GameObjectList[i].transform.rotation = Quaternion.Euler((float)item.Rotation.x, (float)item.Rotation.y, (float)item.Rotation.z);
-                            Debug.Log(GameObjectList[i].name + " " + item.Name);
-                        }
-                    }
-                }
-
-                //myCity = importerModel.City + "," + importerModel.Country;
-                // importerModel = JsonUtility.FromJson<ImporterModel>(jsonFile);
-
-            }
+            jsonFile = reader.text;
         }
         else
         {
-            Debug.Log("File does not exist.");
-            jsonFile = null;
-            Debug.Log("Reverting to Hardcoded Machines Location");
-
+            jsonFile = File.ReadAllText(path);
         }
+
+
+        Debug.Log(path);
+
+        /* if (File.Exists(path))
+         {
+             jsonFile = File.ReadAllText(path);
+             if (jsonFile != null)
+             {*/
+        //int u = 0;
+        rootObject = JsonUtility.FromJson<RootObject>(jsonFile);
+
+        for (int i = 0; i < GameObjectList.Length; i++)
+        {
+
+            foreach (Machinelist item in rootObject.machinelist)
+            {
+                if (GameObjectList[i].name == item.Name)
+                {
+                    GameObjectList[i].transform.position = new Vector3((float)item.Position.x, (float)item.Position.y, (float)item.Position.z);
+                    GameObjectList[i].transform.rotation = Quaternion.Euler((float)item.Rotation.x, (float)item.Rotation.y, (float)item.Rotation.z);
+                    Debug.Log(GameObjectList[i].name + " " + item.Name);
+                }
+            }
+        }
+
+        //myCity = importerModel.City + "," + importerModel.Country;
+        // importerModel = JsonUtility.FromJson<ImporterModel>(jsonFile);
+
+        /* }
+     }
+     else
+     {
+         Debug.Log("File does not exist.");
+         jsonFile = null;
+         Debug.Log("Reverting to Hardcoded Machines Location");
+
+     }*/
     }
 
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
