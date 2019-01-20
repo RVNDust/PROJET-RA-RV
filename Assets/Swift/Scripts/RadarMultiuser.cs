a1ba04bee3a305388d5a33e55e9338f5ae9b16d1
PaerCode <paer763@gmail.com>
2019-01-20 14:59:58 +0100

    Multiplayer Radar Phase  1 Missing Border icons

---
+++ Assets/Swift/Scripts/RadarMultiuser.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class RadarMultiuser : NetworkBehaviour
+{
+    public Material PlayerLocalMat;
+    public GameObject GameObjectLocalPlayerColor;
+    public GameObject RadarObject;
+    public GameObject BlipPrefab;
+    private GameObject MyBlip;
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Radar isLocalPlayer:" + isLocalPlayer);
+
+
+        GetComponent<ThirdPersonUserControl>().enabled = isLocalPlayer;
+        if (isLocalPlayer)
+        {
+            try
+            {
+                // Change the material of the Ethan Glasses
+
+                GameObjectLocalPlayerColor.GetComponent<Renderer>().material = PlayerLocalMat;
+                MyBlip = Instantiate(BlipPrefab, GameObjectLocalPlayerColor.transform.position, GameObjectLocalPlayerColor.transform.rotation);
+                MyBlip.GetComponent<Renderer>().material = PlayerLocalMat;
+                MyBlip.transform.SetParent(gameObject.transform);
+                Destroy(GameObjectLocalPlayerColor);
+                RadarObject.SetActive(true);
+
+
+                /*RadarCamera.transform.parent = transform;
+                RadarCamera.SetActive(true);*/
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+    }
+
+
+
+}
+
