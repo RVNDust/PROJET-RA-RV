48be165b61d0b04ec85d81897caaeb9825f9e95a
PaerCode <paer763@gmail.com>
2019-01-21 19:12:26 +0100

    Radar Working on Clients but not Servers Still needs some work

--- Assets/Swift/Scripts/Radar.cs
+++ Assets/Swift/Scripts/Radar.cs
@@ -19,18 +19,24 @@ public class Radar : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        previousNum = 1;
         getRadarObjects();
     }
 
-
     void getRadarObjects()
     {
+        if (radarObjects != null || radarObjects.Count != 0)
+        {
+            for (int i = 0; i < radarObjects.ToArray().Length; i++)
+            {
+                Destroy(borderObjects[i]);
+                Destroy(radarObjects[i]);
+            }
+        }
+
         previousNum = GameObject.FindGameObjectsWithTag("Players").Length;
         foreach (GameObject fooObj in GameObject.FindGameObjectsWithTag("Players"))
         {
             Debug.Log(GameObject.FindGameObjectsWithTag("Players").Length);
-            int num = 1;
             if (fooObj.GetComponent<NetworkIdentity>().isLocalPlayer == true)
             {
                 Debug.Log("Detected Local Player");
@@ -42,27 +48,25 @@ public class Radar : MonoBehaviour
                 trackedObjects.Add(fooObj);
                 Debug.Log("Detected player:" + fooObj);
             }
-
         }
         radarObjects = new List<GameObject>();
         borderObjects = new List<GameObject>();
+
         foreach (GameObject o in trackedObjects)
         {
             int name = 1;
             GameObject k;
             k = Instantiate(radarPrefab, o.transform.position, o.transform.rotation * Quaternion.Euler(90, 0, 0));
-            k.name = k.name + name;
+            k.name = "Arrow";
             k.transform.parent = o.transform;
-            string TargetName = k.name;
-            
+
             radarObjects.Add(k);
 
             GameObject j;
             j = Instantiate(radarPrefab, o.transform.position, o.transform.rotation * Quaternion.Euler(90, 0, 0));
-            j.name = j.name + name;
+            j.name = "Border";
             j.transform.parent = o.transform;
             j.GetComponent<Renderer>().material = myMat;
-            string TargetName1 = j.name;
 
             borderObjects.Add(j);
             name++;
@@ -74,18 +78,19 @@ public class Radar : MonoBehaviour
 
         for (int i = 0; i < radarObjects.ToArray().Length; i++)
         {
-            if (Vector3.Distance(radarObjects.ToArray()[i].transform.position, transform.position) > switchDistance)
+            if (Vector3.Distance(radarObjects[i].transform.position, transform.position) > switchDistance)
             {
                 Blip.LookAt(radarObjects[i].transform);
                 borderObjects[i].transform.position = transform.position + switchDistance * Blip.forward;
                 Debug.Log("il est passé par ici!");
-                borderObjects[i].layer = LayerMask.NameToLayer("RadarBlips");
+                //borderObjects[i].layer = LayerMask.NameToLayer("RadarBlips");
                 radarObjects[i].layer = LayerMask.NameToLayer("Invisible");
+                // borderObjects[i].transform.parent = transform;
             }
-            else
+            if (Vector3.Distance(radarObjects[i].transform.position, transform.position) < switchDistance)
             {
                 radarObjects[i].layer = LayerMask.NameToLayer("RadarBlips");
-                borderObjects[i].layer = LayerMask.NameToLayer("Invisible");
+                //borderObjects[i].layer = LayerMask.NameToLayer("Invisible");
                 Debug.Log("Et repassera par la!");
             }
         }
